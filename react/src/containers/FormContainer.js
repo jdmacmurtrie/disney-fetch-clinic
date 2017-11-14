25a0c4a7d0c7d7a005332769e501f76c63008838
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-11-14 11:31:38 -0500

    starting code

--- react/src/containers/FormContainer.js
+++ react/src/containers/FormContainer.js
@@ -5,62 +5,31 @@ class FormContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movieTitle: '',
-      movieReleaseYear: '',
-      movieRuntime: ''
     }
-    this.handleChange = this.handleChange.bind(this)
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
     this.handleClear = this.handleClear.bind(this)
   }
 
-  handleChange(event) {
-    let value = event.target.value
-    let name = event.target.name
-    this.setState({ [name]: value })
-  }
-
   handleFormSubmit(event) {
-    event.preventDefault()
-    let formPayload = {
-      title: this.state.movieTitle,
-      release_year: this.state.movieReleaseYear,
-      runtime: this.state.movieRuntime
-    }
-    this.props.addNewMovie(formPayload)
-    this.handleClear(event)
   }
 
   handleClear(event) {
-    debugger
-    event.preventDefault()
-    this.setState({
-      movieTitle: '',
-      movieReleaseYear: '',
-      movieRuntime: ''
-    })
   }
 
   render() {
     return(
       <form className='callout' onSubmit={this.handleFormSubmit}>
         <TextInputField
-          value={this.state.movieTitle}
           label="Movie Title"
           name="movieTitle"
-          handleChange={this.handleChange}
         />
         <TextInputField
-          value={this.state.movieReleaseYear}
           label="Movie Release Year"
           name="movieReleaseYear"
-          handleChange={this.handleChange}
         />
         <TextInputField
-          value={this.state.movieRuntime}
           label="Movie Runtime (in minutes)"
           name="movieRuntime"
-          handleChange={this.handleChange}
         />
 
         <div className="button-group">
--- react/src/containers/MoviesContainer.js
+++ react/src/containers/MoviesContainer.js
@@ -8,31 +8,6 @@ class MoviesContainer extends Component {
     this.state = {
       movies: []
     }
-    this.addNewMovie = this.addNewMovie.bind(this)
-  }
-
-  componentDidMount() {
-    fetch('/api/v1/movies')
-    .then(response => response.json())
-    .then(body => {
-      let allMovies = body.movies
-      // let olderMovies = allMovies.filter(movie =>
-      //   movie.release_year < 1960
-      // )
-      this.setState({ movies: allMovies })
-    })
-  }
-
-  addNewMovie(formPayload) {
-    fetch('/api/v1/movies', {
-      method: 'POST',
-      body: JSON.stringify(formPayload)
-    })
-    .then(response => response.json())
-    .then(body => {
-      let newMovieList = this.state.movies.concat(formPayload)
-      this.setState({ movies: newMovieList })
-    })
   }
 
   render() {
